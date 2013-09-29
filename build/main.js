0427d201a46b0d0b668e39a5e6dec0d4d35e0b22
Rob Halff <rob.halff@gmail.com>
2013-09-29 22:45:53 +0200

    done for now

--- build/main.js
+++ build/main.js
@@ -33,22 +33,18 @@ PackageNavigator = can.Control.extend({
        this.element.find('#packageListNav, .nodes').hide();
     },
     "#packageList button click": function (el, ev) {
+
        // hide package list
        this.element.find('#packageList').hide();
        
-       // show nodeDefinitions in this package.
-       //
-       
-       // show back navigation
-       // ok, how to get the namespace and the name of the package we have clicked?
-       console.log(el.data('ns'));
-       console.log(el.data('package'));
        var str = can.view.render("views/nodes.ejs", { nodes: this.options.nodes[el.data('ns')] });
+       // ok, onClick I want to add a model to the current list of nodes in the graph/flow
+       // then this component is done. The other Controller must take care of what to do
+       // when a node was added to the graph itself. I even should be able to add a node myself from
+       // the console to the nodeList, and see the node appear within the graph.
        var node = this.options.packages[el.data('ns')];
        this.element.find('#packageListNav').show().find('span').html(node.description);
        this.element.find('.nodes').html(str).show();
-
-       console.log(el);
     }
   }
 );
@@ -85,9 +81,4 @@ module.exports = {
 };
 
 },{}]},{},[1])
-;(function(window) {
-can.view.preload('views_navigator_ejs',can.EJS(function(_CONTEXT,_VIEW) { with(_VIEW) { with (_CONTEXT) {var ___v1ew = [];___v1ew.push("<div id=\"packageListNav\" style=\"display: none\">\n  <button class=\"btn\">&lt;&lt;</button> <span>Label</span>\n</div>\n<div id=\"packageList\">\n");___v1ew.push(can.view.txt(0,'div',0,this,function(){var ___v1ew = []; for(var key in packages) { ___v1ew.push("\n<button class=\"btn package\" type=\"button\" data-ns=\"");___v1ew.push(can.view.txt(1,'button','data-ns',this,function(){ return key}));___v1ew.push("\"",can.view.pending(),">");___v1ew.push(can.view.txt(1,'button',0,this,function(){ return key}));___v1ew.push("</button>\n"); } ;return ___v1ew.join('')}));
-___v1ew.push("\n</div>\n<div class=\"nodes\" style=\"display: none\">\n</div>\n<hr />\n");; return ___v1ew.join('')}} }));
-can.view.preload('views_nodes_ejs',can.EJS(function(_CONTEXT,_VIEW) { with(_VIEW) { with (_CONTEXT) {var ___v1ew = [];___v1ew.push(can.view.txt(0,'',0,this,function(){var ___v1ew = []; for(var key in nodes) { ___v1ew.push("\n<button class=\"btn package\" type=\"button\">");___v1ew.push(can.view.txt(1,'button',0,this,function(){ return key}));___v1ew.push("</button>\n"); } ;return ___v1ew.join('')}));
-___v1ew.push("\n");; return ___v1ew.join('')}} }));
-})(this);
+;
