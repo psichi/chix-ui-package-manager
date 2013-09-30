a1eab08cb02f0a00980ea2af88f056f8b828de72
Rob Halff <rob.halff@gmail.com>
2013-09-30 08:17:56 +0200

    get touchy

---
+++ test.js
@@ -0,0 +1,15 @@
+var MODEL = require('chix-ui-models'),
+PackageNavigator = require('./lib/ui');
+
+$.when(
+  $.get('http://serve.chix.io/nodes'),
+  $.get('http://serve.chix.io/packages')).
+  then(function (nodes, pkgs) {
+
+  new PackageNavigator($('#packages'), {
+    nodes: nodes[0],
+    packages: pkgs[0] 
+  });
+
+});
+
